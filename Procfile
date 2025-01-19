81cd9345c5aaa6b4a3b5182c4077d65b0a3650ee
KoteKote1534 <uheosjuygoi.15341582@gmail.com>
2025-01-19 23:34:14 +0900

    torchでcpu
    torchでcpu

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: ./compile.sh && gunicorn app:app
---
+++ pyxel_examples/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip install --no-cache-dir -r requirements.txt \
+    --index-url https://download.pytorch.org/whl/cpu
